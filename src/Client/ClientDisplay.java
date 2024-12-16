bf38eb18811583f5381a5db09c5d79c45e806b90
MeatballsCalls <klmnkehler@gmail.com>
2024-12-16 11:26:23 -0600

    Add files via upload
    added what we made in class

---
+++ src/Client/ClientDisplay.java
@@ -0,0 +1,13 @@
+package main;
+
+import javax.swing.*;
+
+public class ClientDisplay extends JFrame{
+	private JPanel panel;
+	
+	public ClientDisplay() {
+		setBounds(0, 0, 800, 600);
+		setVisible(true);
+		
+	}
+}
